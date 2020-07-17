b3e083c62f2978b644b39a88819709b655ebca26
Max Brown <max.brown@ed.ac.uk>
2020-07-17 08:46:48 +0100

    Update shell script and add urls

--- KewSID/tidyUrls.bash
+++ KewSID/tidyUrls.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# first run the python script
+python scrape_SID.py > allSpeciesUrls.txt
+
+# then filter out unwanted or duplicated URLs
 grep -F "https://data.kew.org/sid/SidServlet?ID=" ./allSpeciesUrls.txt | sort -u

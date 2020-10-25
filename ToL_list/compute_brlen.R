fd9d94a34fc95c079350ada2480a71b30eec08dd
Max Brown <max.brown@ed.ac.uk>
2020-10-25 16:27:13 +0000

    Update links

--- ToL_list/compute_brlen.R
+++ ToL_list/compute_brlen.R
@@ -1,6 +1,6 @@
 library(ape); library(phytools)
 x <- read.tree(file = "./tree.newick")
-tree <- ape::read.tree(file = "~/OneDrive - University of Edinburgh/web_mining/ToL_list/tree.newick")
+tree <- ape::read.tree(file = "./tree.newick")
 tree <- ape::compute.brlen(tree)
 tree <- phytools::force.ultrametric(tree, method = "extend")
-write.tree(tree, "~/OneDrive - University of Edinburgh/web_mining/ToL_list/tree_brlen.newick")
+write.tree(tree, "./tree_brlen.newick")

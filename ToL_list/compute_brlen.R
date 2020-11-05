a20ae9e0044cacaa04bd67036fd23bf733ab5e6d
Max Brown <max.brown@ed.ac.uk>
2020-11-05 22:46:22 +0000

    Drop problematic tips

--- ToL_list/compute_brlen.R
+++ ToL_list/compute_brlen.R
@@ -3,4 +3,9 @@ x <- read.tree(file = "./tree.newick")
 tree <- ape::read.tree(file = "./tree.newick")
 tree <- ape::compute.brlen(tree)
 tree <- phytools::force.ultrametric(tree, method = "extend")
+# drop problematic tips. 
+# Bombusterrestris(indomainBacteria)ott5901391; Aporiacrataegi(speciesinNucletmycea)ott4047122
+tree <- ape::drop.tip(phy = tree, tip = c("Bombusterrestris(indomainBacteria)ott5901391",
+                                  "Aporiacrataegi(speciesinNucletmycea)ott4047122"))
+
 write.tree(tree, "./tree_brlen.newick")
